0263fce3e11369e5592d90674186b57e1924d1e1
lshep <lori.shepherd@roswellpark.org>
2020-02-07 11:23:02 -0500

    Update mysql to RW not readonly

--- start.sql
+++ start.sql
@@ -2,3 +2,5 @@ drop database if exists annotationhub;
 create database annotationhub;
 grant all privileges on annotationhub.* to 'hubuser'@'%' identified by 'MYSQL_REMOTE_PASSWORD' with grant option;
 flush privileges;
+grant all privileges on annotationhub.* to 'ahuser'@'%' identified by 'MYSQL_REMOTE_PASSWORD' with grant option;
+flush privileges;
